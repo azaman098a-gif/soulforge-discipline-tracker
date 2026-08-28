6dfda1ea5ce87a3c6faa0668b4984928bdf65f5a
azaman098a-gif <azaman098a@gmail.com>
2026-08-28 21:43:25 +0700

    Add files via upload

---
+++ assets/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "soulforge-v2";
+
+const BASE = "/soulforge-discipline-tracker/";
+
+const ASSETS = [
+  BASE,
+  BASE + "index.html",
+  BASE + "manifest.webmanifest",
+  BASE + "icon-192.png",
+  BASE + "icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
